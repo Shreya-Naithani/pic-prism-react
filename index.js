115bb619f3f938dcd5a9ba09aebc871a03074f92
Shreya-Naithani <naithanishreya7409@gmail.com>
2024-07-05 01:47:58 +0530

    day5 completed

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+ const app = express();
+
+ const port = process.env.PORT || 3000;
+
+app.get('/',(req,res)=>{
+    res.json("hello");
+})
+
+ app.listen(port,()=>{
+    console.log(`Server is running at port ${port}`)
+ }
+    
+ )

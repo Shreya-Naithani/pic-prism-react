231947a690fc9e14a7ea7c700bd266396515c02b
Shreya-Naithani <naithanishreya7409@gmail.com>
2024-07-11 11:11:44 +0530

    day10 completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import {BrowserRouter} from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import GsapTransition from './Components/GsapTransition';
+import {Provider} from 'react-redux';
+import store from './Redux/Store/store';
 
 
 function App() {
   return (
    <>
+   <Provider store={store}>
    <BrowserRouter>
    <Navbar/>
    <GsapTransition/>
    <Footer/>
    </BrowserRouter>
+   </Provider>
    </>
   )
 }
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,15 +1,42 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import {toast} from 'react-hot-toast';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import authSlice, { login } from '../Redux/Slices/authSlice';
+
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const[email,setEmail] = useState("");
   const[password ,setPassword] = useState("");
+
+const handleLogin = async(e)=>{
+e.preventDefault();
+try {
+  const res = await axios.post("http://localhost:4000/api/login",{
+    email,
+    password
+  })
+  const data = res.data;
+  console.log(data);
+  toast.success(data.message);
+  //dispatch
+  dispatch(login(data));
+  navigate(`/${data.role}/profile`);
+} catch (error) {
+  toast.error(error.response.data.message);
+}
+}
+
+
   return (
     <div className='mt-20 sm:mt-10 min-h-screen flex justify-center items-center w-full'>
       <div className='w-full sm:w-[50vw] md:w-[30vw] rounded-3xl shadow-md px-5 py-6 bg-white'>
 
       <h3 className='text-2xl font-bold text-center mb-4'>Lets connect!</h3>
-      <form>
+      <form onSubmit={handleLogin}>
         
        {/* email */}
         <div className='mb-4'>
@@ -17,8 +44,9 @@ const Login = () => {
        <input className='w-full px-3 py-2 rounded-md shadow-md border border-gray-300 focus:outline-none focus:ring-black focus:border-black' type="email" 
        id="email" 
        name="email"
+       required
        value={email}
-       onChange={()=>setEmail(e.target.value)}
+       onChange={(e)=>setEmail(e.target.value)}
        placeholder='abc@gmail.com' />
         </div>
         
@@ -28,9 +56,10 @@ const Login = () => {
        <input className='w-full px-3 py-2 rounded-md shadow-md border border-gray-300 focus:outline-none focus:ring-black focus:border-black'
         type="password" 
         id="password"
+        required
          name="password"
           value={password}
-         onChange={()=>setEmail(e.target.value)}
+         onChange={(e)=>setPassword(e.target.value)}
 
          placeholder='password' />
         </div>
